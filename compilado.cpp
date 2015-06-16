5a52aa853c80695bf1d2ee8f379a3ca54f6adc49
jonesmarxs <jonesmarxs@gmail.com>
2015-06-16 01:15:13 -0300

    merge master

--- compilado.cpp
+++ compilado.cpp
@@ -1,28 +1,2 @@
-#include<stdio.h>
-#include<string.h>
-#include<iostream>
 
-using namespace std;
-
-int main(void)
-{
-	int temp0;
-	temp0 = 2;
-	int temp1;
-	temp1 = temp0;
-	int temp2;
-	temp2 = temp1;
-	int temp3 = temp1 + temp2;
-	cout << temp3 << endl;
-	int temp4 = temp1 < temp2;
-	if(!temp4) goto FIM;
-	int temp5;
-	temp5 = 1;
-	int temp6;
-	temp6 = temp5;
-	cout << temp1 << endl;
-FIM:
-	cout << temp4 << endl;
-	return 0;
-}
-
+VARIAVEL NAO DECLARADA - Linha: 11
